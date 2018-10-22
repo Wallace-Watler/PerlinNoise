84be842ebf1e001f0cc90de9704eb3a758d83ede
Wallace <watlerathome@gmail.com>
2018-10-22 13:08:39 -0500

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate rand;
+
+use rand::prng;
+use rand::rngs;
+
+fn main() {
+    const PROGRAM_SEED: [u8; 32] = [0; 32];
+    const ITERATIONS: u32 = 1;
+    const GRADIENT_DISTANCE: u32 = 10;
+    const AMPLITUDE: f64 = 1.0;
+    const PERIODIC: bool = false;
+    const NUM_GRADIENTS: u32 = 6;
+
+    for x in 0..50 {
+        println!("{} ", perlin_noise_1d(x as f64, ITERATIONS, GRADIENT_DISTANCE, AMPLITUDE, PERIODIC, NUM_GRADIENTS));
+    }
+}
+
+fn perlin_noise_1d(x: f64, iterations: u32, gradient_distance: u32, amplitude: f64, periodic: bool, num_gradients: u32) -> f64 {
+
+}
