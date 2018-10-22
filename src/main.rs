b4feae29653823fbd985829102070dc4b565482c
Wallace Watler <watlerkw@msoe.edu>
2018-10-22 14:43:06 -0500

    It now compiles.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@ extern crate rand;
 
 use rand::prng;
 use rand::rngs;
+use perlin_noise::PerlinNoise1D;
 
 fn main() {
-    const PROGRAM_SEED: [u8; 32] = [0; 32];
+    const SEED: [u8; 32] = [0; 32];
     const ITERATIONS: u32 = 1;
-    const GRADIENT_DISTANCE: u32 = 10;
+    const GRADIENT_DISTANCE: f64 = 10.0;
     const AMPLITUDE: f64 = 1.0;
-    const PERIODIC: bool = false;
     const NUM_GRADIENTS: u32 = 6;
 
-    struct PerlinNoise {
+    let test_perlin_noise_1d = PerlinNoise1D::new(SEED, ITERATIONS, GRADIENT_DISTANCE, AMPLITUDE, NUM_GRADIENTS);
+
+    for x in 0..50 {
+        print!("{} ", test_perlin_noise_1d.perlin_noise_1d(x as f64));
+    }
+}
+
+mod perlin_noise {
+    pub struct PerlinNoise1D {
         seed: [u8; 32],
         iterations: u32,
         gradient_distance: f64,
@@ -20,17 +28,13 @@ fn main() {
         num_gradients: u32
     }
 
-    impl PerlinNoise {
-        fn new(mut self, seed: [u8; 32], iterations: u32, gradient_distance: f64, amplitude: f64, num_gradients: u32) -> PerlinNoise {
-            
+    impl PerlinNoise1D {
+        pub fn new(seed: [u8; 32], iterations: u32, gradient_distance: f64, amplitude: f64, num_gradients: u32) -> PerlinNoise1D {
+            PerlinNoise1D { seed, iterations, gradient_distance, amplitude, periodic: num_gradients == 0, num_gradients }
+        }
+
+        pub fn perlin_noise_1d(&self, x: f64) -> f64 {
+            0.0
         }
     }
-
-    for x in 0..50 {
-        println!("{} ", perlin_noise_1d(x as f64, ITERATIONS, GRADIENT_DISTANCE, AMPLITUDE, PERIODIC, NUM_GRADIENTS));
-    }
-}
-
-fn perlin_noise_1d(x: f64, iterations: u32, gradient_distance: u32, amplitude: f64, periodic: bool, num_gradients: u32) -> f64 {
-
 }
